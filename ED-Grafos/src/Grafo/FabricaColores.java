bc205b2ec9a95431002b0ee68303b2183f7f05df
Juanillo52 <juanillo.box@gmail.com>
2018-12-02 20:32:52 +0100

    Merge origin/master
    Conflicts:
    	ED-Grafos/src/Grafo/Implementacion1Graph.java

--- ED-Grafos/src/Grafo/FabricaColores.java
+++ ED-Grafos/src/Grafo/FabricaColores.java
@@ -5,6 +5,8 @@
  */
 package Grafo;
 
+import java.util.*;
+
 /**
  *
  * @author hecto
@@ -17,4 +19,71 @@ public class FabricaColores {
         colores = new Implementacion1Graph(o);
     }
 
+    public void resolver() {
+        Iterator aristas = colores.edges();
+
+        Edge min = ((Edge) aristas.next()), next;
+        while (aristas.hasNext()) {
+            next = ((Edge) aristas.next());
+            if ((int) next.getElement() < (int) min.getElement()) {
+                min = next;
+            }
+        }
+        int tiempo = (int) min.getElement();
+        Vertex[] aux = colores.endVertices(min);
+        Vertex actual = aux[1];
+        colores.removeVertex(aux[0]);
+        List<Vertex> elementos = new ArrayList();
+        elementos.add(aux[0]);
+        elementos.add(aux[1]);
+
+        Iterator vertices = colores.vertices();
+        boolean enc;
+        int minimoActual = -1;
+        Vertex siguiente = null;
+        while (vertices.hasNext()) {
+
+            aristas = colores.edges();//actualizamos la lista de aristas
+            enc = false;
+            while (aristas.hasNext() && !enc) {
+                next = ((Edge) aristas.next());
+                Vertex[] verticesActuales = colores.endVertices(next);
+                if (verticesActuales[0] == actual) {
+                    minimoActual = (int) next.getElement();
+                    siguiente = verticesActuales[1];
+                    enc = true;
+                }
+            }
+            while (aristas.hasNext()) {
+                next = ((Edge) aristas.next());
+                Vertex[] verticesActuales = colores.endVertices(next);
+                if (verticesActuales[0] == actual) {
+                    int minimoPosible = (int) next.getElement();
+                    if (minimoPosible < minimoActual) {
+                        minimoActual = minimoPosible;
+                        siguiente = verticesActuales[1];
+                    }
+                }
+            }
+            tiempo += minimoActual;
+            elementos.add(siguiente);
+            colores.removeVertex(actual);
+            actual = siguiente;
+            vertices = colores.vertices(); //actualizamos la lista de vertices
+            vertices.next();
+        }
+        int produccion = 1200 - tiempo;
+        System.out.println("El tiempo de producción semanal de pintura es:"
+                + " " + produccion / 60 + " horas y " + produccion % 60 + " minutos");
+        System.out.println("El tiempo de disposición es:"
+                + " " + tiempo / 60 + " horas y " + tiempo % 60 + " minutos");
+        String s = "La secuencia de producción: ";
+        for (int i = 0; i < elementos.size(); i++) {
+            s += elementos.get(i).getId();
+            if (i < elementos.size() - 1) {
+                s += " - ";
+            }
+        }
+        System.out.println(s);
+    }
 }
