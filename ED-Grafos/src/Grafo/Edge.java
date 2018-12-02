c8b8fb46024ddd183ce67d0ca69c5fb6dfaf61c9
EichZz <hectoramm.rubio@gmail.com>
2018-12-02 17:04:51 +0100

    endVertices

--- ED-Grafos/src/Grafo/Edge.java
+++ ED-Grafos/src/Grafo/Edge.java
@@ -13,6 +13,15 @@ public class Edge {
 
     private Object element;
     private Edge next;
+    private Vertex vertex;
+
+    public Vertex getVertex() {
+        return vertex;
+    }
+
+    public void setVertex(Vertex vertex) {
+        this.vertex = vertex;
+    }
 
     public Edge(Object ele) {
         this.element = ele;
